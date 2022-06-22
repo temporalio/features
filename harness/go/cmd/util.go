70b0930d7b2baa36ea80e326f3856ddbc2613f0a
Chad Retz <chad@temporal.io>
2022-06-22 09:42:18 -0700

    Update SDK and add Go workflow CI (#61)

--- harness/go/cmd/util.go
+++ harness/go/cmd/util.go
@@ -5,8 +5,13 @@ import "golang.org/x/mod/semver"
 // GoBuildTags collects the set of tags used by different feature files based
 // on the given SDK version.
 func GoBuildTags(sdkVersion string) (tags []string) {
-	// Add more tags as needed...
+	// When realtive paths are used or an otherwise non-semver version is used, we
+	// need to assume there are no tags
+	if !semver.IsValid(sdkVersion) {
+		return nil
+	}
 
+	// Add more tags as needed...
 	if semver.Compare(sdkVersion, "v1.11.0") < 0 {
 		tags = append(tags, "pre1.11.0")
 	}

b27ddabeef9763fa22b0437b6df7d6225c3e0aa3
Roey Berman <roey.berman@gmail.com>
2022-09-06 12:45:35 -0700

    Fix log statements

--- cmd/build_image.go
+++ cmd/build_image.go
@@ -128,6 +128,7 @@ func (i *ImageBuilder) buildFromRepo(ctx context.Context) error {
 			return err
 		}
 	}
+	i.log.Info("Building from given repo ref", tag.NewStringTag("repoURL", i.config.RepoURL), tag.NewStringTag("repoRef", i.config.RepoRef))
 
 	// We have to clone into rootDir because it's part of the docker context
 	tempDir, err := os.MkdirTemp(i.rootDir, "cloned-repo-*")
@@ -164,7 +165,7 @@ func (i *ImageBuilder) buildFromVersion(ctx context.Context) error {
 		// TODO: python is an exception
 		return fmt.Errorf("expected version to be valid semver")
 	}
-	i.log.Info(version)
+	i.log.Info("Building from given version", tag.NewStringTag("version", version))
 
 	// Build the list of tags: lang-major.minor.patch, and optionally: lang-major.minor, lang-major, lang
 	tags := []string{fmt.Sprintf("%s-%s", i.config.Lang, version[1:])}
@@ -226,7 +227,7 @@ func (i *ImageBuilder) dockerBuild(ctx context.Context, config buildConfig) erro
 	}
 	args = append(args, i.rootDir)
 
-	i.log.Info("building docker image", tag.NewAnyTag("args", args))
+	i.log.Info("Building docker image", tag.NewAnyTag("args", args))
 	dockerBuild := exec.Command("docker", args...)
 	dockerBuild.Stdout = os.Stdout
 	dockerBuild.Stderr = os.Stderr
@@ -252,7 +253,7 @@ func (i *ImageBuilder) gitClone(ctx context.Context, rootDir string) (string, er
 	repoBaseDir := fmt.Sprintf("sdk-%s", expandedLangName)
 	targetDir := filepath.Join(rootDir, repoBaseDir)
 	args := []string{"clone", "--recurse-submodules", i.config.RepoURL, targetDir}
-	i.log.Info("fetching git repo", tag.NewAnyTag("args", args))
+	i.log.Info("Fetching git repo", tag.NewAnyTag("args", args))
 	gitClone := exec.Command("git", args...)
 	gitClone.Stdout = os.Stdout
 	gitClone.Stderr = os.Stderr

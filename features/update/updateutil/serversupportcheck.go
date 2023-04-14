54aa168b761f10dce2c116b66323d431e769d274
Matt McShane <matt.mcshane@temporal.io>
2023-04-14 14:38:20 -0400

    Support more scenarios in the update server check
    Allow a PermissionDenied error either directly returned by the
    UpdateWorkflow call or returned by the Handle to indicate that the
    server does not support Update.

--- features/update/updateutil/serversupportcheck.go
+++ features/update/updateutil/serversupportcheck.go
@@ -9,9 +9,22 @@ import (
 )
 
 func CheckServerSupportsUpdate(ctx context.Context, c client.Client) string {
-	handle, _ := c.UpdateWorkflow(ctx, "fake", "also_fake", "__does_not_exist")
-	err := handle.Get(ctx, nil)
+	handle, err := c.UpdateWorkflow(ctx, "fake", "also_fake", "__does_not_exist")
+	// Newer versions of the sdk will return a permission denied error here
 	var denied *serviceerror.PermissionDenied
+	var notFound *serviceerror.NotFound
+	if errors.As(err, &denied) {
+		return "server support for update is disabled; " +
+			"set frontend.enableUpdateWorkflowExecution=true in dynamic config to enable"
+	}
+	// Otherwise we expect a not found error since the workflowID is fake
+	if errors.As(err, &notFound) {
+		return ""
+	}
+	err = handle.Get(ctx, nil)
+
+	// A few early versions of the sdk will return the permission denied error
+	// here so check for that as well
 	if errors.As(err, &denied) {
 		return "server support for update is disabled; " +
 			"set frontend.enableUpdateWorkflowExecution=true in dynamic config to enable"

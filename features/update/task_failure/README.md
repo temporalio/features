c666933275f3ce47245f5be5270b8853adfd5155
Matt McShane <matt.mcshane@temporal.io>
2023-05-25 11:58:10 -0400

    Modify update tests to demand correct panic behavior
    Update validation panics should fail the update. Update execution panics
    should abide by the WorkflowPanicPolicy.

--- features/update/task_failure/README.md
+++ features/update/task_failure/README.md
@@ -9,5 +9,3 @@ The error received by the caller will be through the handle.Get call and will
 contain the string contents of the panic/exception message. Panics/Exceptions out of validation
 handlers are equivalent to rejections from a durability perspective while panics/exceptions
 from the main execution function are the equivalent of panicing/throwing in a workflow task.
-
-Note: Current Go SDK does not reflect spec
--- features/update/task_failure/feature.go
+++ features/update/task_failure/feature.go
@@ -2,21 +2,29 @@ package task_failure
 
 import (
 	"context"
+	"strings"
 
+	enumspb "go.temporal.io/api/enums/v1"
 	"go.temporal.io/features/features/update/updateutil"
 	"go.temporal.io/features/harness/go/harness"
+	"go.temporal.io/features/harness/go/history"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/temporal"
+	"go.temporal.io/sdk/worker"
 	"go.temporal.io/sdk/workflow"
 )
 
 const (
-	execPanic      = "execPanic"
-	validatePanic  = "validatePanic"
-	shutdownSignal = "shutdown_signal"
+	panicMsg                = "fear not, dear examiner of logs, for this panic was intentional"
+	panicFromExecUpdate     = "panic_from_exec"
+	panicFromValidateUpdate = "panic_from_validate"
+	shutdownSignal          = "shutdown!"
 )
 
 var Feature = harness.Feature{
-	Workflows: Workflow,
+	Workflows:                          PanickyUpdates,
+	WorkerOptions:                      worker.Options{WorkflowPanicPolicy: worker.BlockWorkflow},
+	DisableWorkflowPanicPolicyOverride: true,
 	Execute: func(ctx context.Context, runner *harness.Runner) (client.WorkflowRun, error) {
 		if reason := updateutil.CheckServerSupportsUpdate(ctx, runner.Client); reason != "" {
 			return nil, runner.Skip(reason)
@@ -30,44 +38,78 @@ var Feature = harness.Feature{
 			ctx,
 			run.GetID(),
 			run.GetRunID(),
-			execPanic,
+			panicFromValidateUpdate,
 		)
 		runner.Require.NoError(err)
 		err = handle.Get(ctx, nil)
-		runner.Require.Error(err)
-		runner.Require.ErrorContains(err, "update oops")
+		var panicErr *temporal.PanicError
+		runner.Require.ErrorAs(err, &panicErr)
+		runner.Require.ErrorContains(err, panicMsg)
 
-		handle, err = runner.Client.UpdateWorkflow(
+		_, err = runner.Client.UpdateWorkflow(
 			ctx,
 			run.GetID(),
 			run.GetRunID(),
-			validatePanic,
+			panicFromExecUpdate,
 		)
 		runner.Require.NoError(err)
-		err = handle.Get(ctx, nil)
-		runner.Require.Error(err)
-		runner.Require.ErrorContains(err, "validator oops")
 
 		runner.Require.NoError(runner.Client.SignalWorkflow(ctx, run.GetID(), run.GetRunID(), shutdownSignal, nil))
+		runner.Require.NoError(run.Get(ctx, nil))
+
+		runner.Require.Equal(1, countPanicWFTFailures(ctx, runner),
+			"update handler panic should have caused 1 WFT Failure")
+
 		updateutil.RequireNoUpdateRejectedEvents(ctx, runner)
 		return run, ctx.Err()
 	},
 }
 
-func Workflow(ctx workflow.Context) error {
-	if err := workflow.SetUpdateHandlerWithOptions(ctx, execPanic,
-		func(ctx workflow.Context) (int, error) {
-			panic("update oops")
-		},
-		workflow.UpdateHandlerOptions{},
-	); err != nil {
+func countPanicWFTFailures(ctx context.Context, runner *harness.Runner) int {
+	fetcher := &history.Fetcher{
+		Client:         runner.Client,
+		Namespace:      runner.Namespace,
+		TaskQueue:      runner.TaskQueue,
+		FeatureStarted: runner.CreateTime,
+	}
+	histories, err := fetcher.Fetch(ctx)
+	runner.Require.NoError(err)
+	runner.Require.NotEmpty(histories)
+	count := 0
+	for _, ev := range histories[0].GetEvents() {
+		if attrs := ev.GetWorkflowTaskFailedEventAttributes(); attrs != nil {
+			if strings.Contains(attrs.GetFailure().GetMessage(), panicMsg) &&
+				attrs.GetCause() == enumspb.WORKFLOW_TASK_FAILED_CAUSE_WORKFLOW_WORKER_UNHANDLED_FAILURE {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+var panicUpdate = true
+
+func PanickyUpdates(ctx workflow.Context) error {
+	if err := workflow.SetUpdateHandler(ctx, panicFromExecUpdate, func() error {
+		// DON'T DO THIS. This panicUpdate global is not part of the workflow
+		// state so reading and setting it is non-determinism. We allow
+		// ourselves this transgression in this controlled test setting to
+		// effect an update that panics once and then not on subsequent
+		// invocations.
+		if panicUpdate {
+			panicUpdate = false
+			panic(panicMsg)
+		}
+		return nil
+	}); err != nil {
 		return err
 	}
 
-	if err := workflow.SetUpdateHandlerWithOptions(ctx, validatePanic,
-		func(ctx workflow.Context) error { return nil },
-		workflow.UpdateHandlerOptions{Validator: func() error { panic("validator oops") }},
-	); err != nil {
+	if err := workflow.SetUpdateHandlerWithOptions(ctx, panicFromValidateUpdate, func() error {
+		return nil
+	}, workflow.UpdateHandlerOptions{
+		Validator: func() error { panic(panicMsg) },
+	}); err != nil {
 		return err
 	}
 

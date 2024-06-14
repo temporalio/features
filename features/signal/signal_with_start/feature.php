aad7f6278331d4e806fda3f8a3e4808d398485e6
roxblnfk <roxblnfk@ya.ru>
2024-06-14 23:53:49 +0400

    chore(PHP): add case into the `signal\signal_with_start` feature

--- features/signal/signal_with_start/feature.php
+++ features/signal/signal_with_start/feature.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 namespace Harness\Feature\Signal\SignalWithStart;
 
 use Harness\Attribute\Check;
+use Harness\Attribute\Stub;
 use Harness\Runtime\Feature;
 use Temporal\Client\WorkflowClientInterface;
 use Temporal\Client\WorkflowOptions;
+use Temporal\Client\WorkflowStubInterface;
+use Temporal\Workflow;
 use Temporal\Workflow\SignalMethod;
 use Temporal\Workflow\WorkflowInterface;
 use Temporal\Workflow\WorkflowMethod;
@@ -19,8 +22,12 @@ class FeatureWorkflow
     private int $value = 0;
 
     #[WorkflowMethod('Workflow')]
-    public function run(): int
+    public function run(int $arg = 0)
     {
+        $this->value += $arg;
+
+        yield Workflow::await(fn() => $this->value > 0);
+
         return $this->value;
     }
 
@@ -34,7 +41,7 @@ class FeatureWorkflow
 class FeatureChecker
 {
     #[Check]
-    public static function check(
+    public static function checkSignalProcessedBeforeHandler(
         WorkflowClientInterface $client,
         Feature $feature,
     ): void {
@@ -42,8 +49,27 @@ class FeatureChecker
             FeatureWorkflow::class,
             WorkflowOptions::new()->withTaskQueue($feature->taskQueue),
         );
-        $run = $client->startWithSignal($stub, 'add', [42]);
+        $run = $client->startWithSignal($stub, 'add', [42], [1]);
+
         // See https://github.com/temporalio/sdk-php/issues/457
-        Assert::same($run->getResult(), 42, 'Signal must be executed before Workflow handler');
+        Assert::same($run->getResult(), 43, 'Signal must be processed before WF handler. Result: ' . $run->getResult());
+    }
+
+    #[Check]
+    public static function checkSignalToExistingWorkflow(
+        #[Stub('Workflow', args: [-2])] WorkflowStubInterface $stub,
+        WorkflowClientInterface $client,
+        Feature $feature,
+    ): void {
+        $stub2 = $client->newWorkflowStub(
+            FeatureWorkflow::class,
+            WorkflowOptions::new()
+                ->withTaskQueue($feature->taskQueue)
+                // Reuse same ID
+                ->withWorkflowId($stub->getExecution()->getID()),
+        );
+        $run = $client->startWithSignal($stub2, 'add', [42]);
+
+        Assert::same($run->getResult(), 40, 'Existing WF must be reused. Result: ' . $run->getResult());
     }
 }

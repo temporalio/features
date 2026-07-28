ae57b12ac95320cc84a19085a59016d5e62cf62d
Lenny Chen <lenny.chen@temporal.io>
2026-07-27 17:03:26 -0700

    Add Worker snippets for the SDK Worker pages
    Adds snippet regions used by the Run a Worker pages in the docs:
    
    - go-versioned-worker
    - python-create-worker, python-versioned-worker, python-worker-graceful-shutdown
    - typescript-versioned-worker, typescript-worker-graceful-shutdown
    
    Placeholder Workflow and Activity names follow the existing
    snippets/plugins convention (HelloWorkflow, some_activity).

--- features/snippets/worker/worker.go
+++ features/snippets/worker/worker.go
@@ -3,7 +3,12 @@ package worker
 import (
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
+	"go.temporal.io/sdk/workflow"
 )
+
+func HelloWorkflow(ctx workflow.Context, name string) (string, error) {
+	return "Hello, " + name + "!", nil
+}
 
 func Run() error {
 	c, err := client.Dial(client.Options{
@@ -17,6 +22,34 @@ func Run() error {
 	// @@@SNIPSTART go-worker-max-cached-workflows
 	worker.SetStickyWorkflowCacheSize(0)
 	w := worker.New(c, "task-queue", worker.Options{})
+	// @@@SNIPEND
+
+	return w.Run(worker.InterruptCh())
+}
+
+func RunVersioned() error {
+	c, err := client.Dial(client.Options{
+		HostPort: "localhost:7233",
+	})
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	// @@@SNIPSTART go-versioned-worker
+	w := worker.New(c, "my-task-queue", worker.Options{
+		DeploymentOptions: worker.DeploymentOptions{
+			UseVersioning: true,
+			Version: worker.WorkerDeploymentVersion{
+				DeploymentName: "my-app",
+				BuildID:        "1.0",
+			},
+		},
+	})
+
+	w.RegisterWorkflowWithOptions(HelloWorkflow, workflow.RegisterOptions{
+		VersioningBehavior: workflow.VersioningBehaviorPinned,
+	})
 	// @@@SNIPEND
 
 	return w.Run(worker.InterruptCh())

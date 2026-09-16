399dc433d1c1041b7cb823d5f2ac6caa366d1284
Lenny Chen <lenny.chen@temporal.io>
2026-09-16 13:29:49 -0700

    Format Java threshold snippet

--- features/snippets/external_storage/threshold/threshold_config.java
+++ features/snippets/external_storage/threshold/threshold_config.java
@@ -4,10 +4,7 @@ import io.temporal.payload.storage.StorageDriver;
 class ThresholdConfigSnippet {
   static ExternalStorage configure(StorageDriver driver) {
     // @@@SNIPSTART java-external-storage-threshold
-    return ExternalStorage.newBuilder()
-        .setDriver(driver)
-        .setPayloadSizeThreshold(0)
-        .build();
+    return ExternalStorage.newBuilder().setDriver(driver).setPayloadSizeThreshold(0).build();
     // @@@SNIPEND
   }
 }
